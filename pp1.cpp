54f42e69fd7b21ca193def2cecd09f5ae9320a41
Robert McAnulty <r_mcanulty@yahoo.com>
2014-02-05 13:12:34 -0500

    PP1 Initial

---
+++ pp1.cpp
@@ -0,0 +1,66 @@
+/*
+ * CSCI 21 Programming Project #1
+ * Our first project, creating functions to cover a variety of C++ code.
+ *
+ * Robert McAnulty
+ * Date created: 05Feb14
+ * Last date modified: 05Feb14
+ *
+ * SOURCES: cpp_starter.txt
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/* 
+    count the number of alphabetic characters and the number of numeric characters in a string (function name: countCharacters; parameters: string,     int&, int&; return value: void)
+*/
+void countCharacters (string ccstring, int& cc1, int& cc2)
+    {
+        
+    }
+
+/*
+    capitalize/lowercase every other character in a string -- first character of the string should be capitalized, the second character lowercase,      etc. (function name: upAndDown; parameter: string; return value: string)
+*/
+string upAndDown (string uadstring)
+    {
+        
+    }
+
+/*
+    count the number of words--delimited by space characters--in a string; assume that the parameter will never have multiple spaces back-to-back,      and will never begin or end with spaces (function name: countWords; parameter: string; return value: int)
+*/
+int countWords (string cwstring)
+    {
+        
+    }
+
+/*
+    compute the average of the values in an array; assume the function will never receive an array of size 0 (function name: computeAverage;            parameters: int[], int; return value: int;
+*/
+int computeAverage (int[] ca1, int ca2);
+
+/*
+    identify the smallest value in an array; assume the function will never receive an array of size 0 (function name: findMinValue; parameters:        int[], int; return value: int)
+*/
+int findMinValue (int[] fminv1, int fminv2);
+
+/*
+    identify the largest value in an array; assume the function will never receive an array of size 0 (function name: findMaxValue; parameters: int[], int; return value: int)
+*/
+int findMaxValue (int[] fmaxv1, int fmaxv2);
+
+/*
+ * This is the program's main function/entry point.
+ */
+int main ()
+{
+	
+	// print a welcome message to the screen
+	cout << "Welcome to the program!\n\n";
+	
+	return 0;
+
+}
