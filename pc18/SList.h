e6943964d71ffd951b081735118bc1143e042ab7
Robert McAnulty <r_mcanulty@yahoo.com>
2014-04-09 13:40:32 -0400

    PC18 Cleaned up and PP3 Initial

--- pc18/SList.h
+++ pc18/SList.h
@@ -11,21 +11,17 @@ class SLList
 
 public:
 
-	SLList();//sets values head(NULL) and size(0)
-	virtual ~SLList();// calls the clear function
+	SLList();
+	virtual ~SLList();
 	
-	void insertHead (int value);//create a new SLNode and attach as head of list
-	void removeHead ();//remove the head node from the list
+	void insertHead (int value);
+	void removeHead ();
 	
-	void clear ();//clear the entire contents of the list, freeing all memory
-	             //associated with all nodes
+	void clear ();
 	
 	unsigned int getSize () const;
 	
-	string toString () const;//return a string representation of the contents
-	                         // of all nodes in the list, in the format
-	                         // NUM1,NUM2,...,LASTNUM; return empty string on 
-	                         // empty list
+	string toString () const;
 	                         
 private:
 
