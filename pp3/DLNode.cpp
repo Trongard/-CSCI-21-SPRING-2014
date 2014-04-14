420602db96a0054b16d4d4f67333fa6cded54357
Robert McAnulty <r_mcanulty@yahoo.com>
2014-04-14 13:27:50 -0400

    PP3 Changed folder name

---
+++ pp3/DLNode.cpp
@@ -0,0 +1,46 @@
+#include "DLNode.h"
+
+DLNode::DLNode ()
+{
+    
+}
+
+DLNode::DLNode (int newContents)
+{
+    
+}
+
+DLNode::~DLNode ()
+{
+    
+}
+
+void DLNode::setContents (int newContents)
+{
+    
+}
+
+void DLNode::setNext (DLNode* newNext)
+{
+    
+}
+
+void DLNode::setPrevious (DLNode* newPrevious)
+{
+    
+}
+
+int DLNode::getContents () const
+{
+    
+}
+
+DLNode* DLNode::getNext () const
+{
+    
+}
+
+DLNode* DLNode::getPrevious () const
+{
+    
+}
---
+++ pp3/DLNode.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class DLNode
+{
+    public:
+        DLNode ();
+        DLNode (int newContents);
+        
+        virtual ~DLNode ();
+        
+        void setContents (int newContents);
+        void setNext (DLNode* newNext);
+        void setPrevious (DLNode* newPrevious);
+        
+        int getContents () const;
+        DLNode* getNext () const;
+        DLNode* getPrevious () const;
+        
+    private:
+        DLNode* next;
+        DLNode* previous;
+        int contents;
+        
+}
