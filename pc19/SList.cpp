362bbe73c4ebd05688e2b0f5cfa5f36f7a82f7da
Robert McAnulty <r_mcanulty@yahoo.com>
2014-04-14 11:34:17 -0400

    PC19 Initial

---
+++ pc19/SList.cpp
@@ -0,0 +1,55 @@
+#include"SList.h"
+#include "SLNode.h"
+
+#include <iostream>
+using namespace std;
+
+SList::SList()
+{
+    
+}// sets values head(NULL) and size(0)
+	
+SList::~SList()
+{
+    
+}// calls the clear function
+
+void SList::insertHead (int)
+{
+    
+}// create a new SLNode and attach as head of list
+
+void SList::insertTail (int)
+{
+    
+}// create a new SLNode and attach at the end of list
+
+void SList::removeHead ()
+{
+    
+}//remove the head node from the list
+
+void SList::removeTail ()
+{
+    
+}//remove the tail node from the list
+
+void SList::clear ()
+{
+    
+}//clear the entire contents of the list, freeing all memory
+               //associated with all nodes
+
+unsigned int SList::getSize () const
+{
+    
+}
+
+string SList::toString () const
+{
+    
+}//return a string representation of the contents
+                          //of all nodes in the list, in the format
+                          //NUM1,NUM2,...,LASTNUM; return empty string on 
+                          //empty list
+                          
