82db45ad46ba0b50dac1ddbedc71197d99f24475
Robert McAnulty <r_mcanulty@yahoo.com>
2014-04-14 13:47:12 -0400

    PP3 Updated

--- pp3/DLNode.cpp
+++ pp3/DLNode.cpp
@@ -1,11 +1,13 @@
 #include "DLNode.h"
 
 DLNode::DLNode ()
+:contents(0), next(NULL), previous(NULL)
 {
     
 }
 
 DLNode::DLNode (int newContents)
+:contents(newContents), next(NULL), previous(NULL)
 {
     
 }
@@ -17,30 +19,30 @@ DLNode::~DLNode ()
 
 void DLNode::setContents (int newContents)
 {
-    
+    contents = newContents;
 }
 
 void DLNode::setNext (DLNode* newNext)
 {
-    
+    next = newNext;
 }
 
 void DLNode::setPrevious (DLNode* newPrevious)
 {
-    
+    previous = newPrevious;
 }
 
 int DLNode::getContents () const
 {
-    
+    return contents;
 }
 
 DLNode* DLNode::getNext () const
 {
-    
+    return next;
 }
 
 DLNode* DLNode::getPrevious () const
 {
-    
+    return previous;
 }
---
+++ pp3/pp3.cpp
@@ -0,0 +1,23 @@
+/*
+ * CSCI 21 Programming Project #3
+ * Using a double-linked list to create a dynamically linked data structure.
+ *
+ * Robert McAnulty
+ * Date created: 14Apr14
+ * Last date modified: 14Apr14
+ *
+ * SOURCES: project-3-unit-test.cpp, various projects and challenges of mine, etc.
+ */
+ 
+#include "DLList.h"
+#include "DLNode.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
